81a132d3026801ff941f86b3bb40152d9c71e77f
ynishimura <ynishimura@mis.doshisha.ac.jp>
2016-10-20 23:26:23 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-yum -y update
+
+# アプリケーションの存在確認
+which ansible-playbook > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo "Yeah！"
+else
+    sudo yum install -y epel-release && sudo yum install -y ansible
+fi
