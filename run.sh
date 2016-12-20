d97afac778ea5b8d7d6af6b2acdac7e3d01d2d98
ynishimura <ynishimura@mis.doshisha.ac.jp>
2016-12-21 00:52:15 +0900

    update

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 # アプリケーションの存在確認
 which ansible-playbook > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    echo "Yeah！"
+    ansible-playbook main.yml --connection=local
 else
     sudo yum install -y epel-release && sudo yum install -y ansible
 fi
